91ef39ae3780a41d20b315124151032f429eb8a3
gim-ihwan <gim-ihwan@192.168.1.151>
2018-09-04 23:12:17 +0900

    + Hi World

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -3,8 +3,10 @@ public class helloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello WOrld");
 		System.out.println("Hello WOrld");
+		System.out.println("Hi WOrld");
 		System.out.println("Hello WOrld");
 		System.out.println(Math.abs(-10));
 		System.out.println(Math.PI);
+		
 	}
 }
